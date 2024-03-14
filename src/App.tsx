0905ef5145bdab0edcc8e62ff228533c2e72dc1b
rugefx <zackfxg@gmail.com>
2024-03-14 23:56:11 +0700

    chore: man im tweakin or sum i dont even need router in this project

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <main>
+      <h1>RugeFX</h1>
+    </main>
+  );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { routeTree } from "./routeTree.gen";
-import { RouterProvider, createRouter } from "@tanstack/react-router";
+import App from "./App";
 import "./index.css";
-
-const router = createRouter({ routeTree });
-
-declare module "@tanstack/react-router" {
-  interface Register {
-    router: typeof router;
-  }
-}
 
 const rootElement = document.getElementById("root")!;
 if (!rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement);
   root.render(
     <React.StrictMode>
-      <RouterProvider router={router} />
+      <App />
     </React.StrictMode>
   );
 }
